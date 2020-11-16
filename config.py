6cf950695ffa28eb26b98544e2edb8c11c11ecbb
Sumit Mishra <46062583+SumitM0432@users.noreply.github.com>
2020-11-16 13:19:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 batch_size = 512
-epochs = 10
+epochs = 100
 input_shape = [21]
 patience = 5
-min_delta = 0.00
+min_delta = 0.001
 TRAINING_FILE = "input/hotel_bookings.csv"
 MODEL_OUTPUT = "models/"
